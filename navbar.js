70591ba299db0846f4d22725138840de28a3fdfe
abhizeit <abhijeitsarraf@gmail.com>
2022-08-27 13:32:30 +0530

    updated navbar.js file

--- navbar.js
+++ navbar.js
@@ -1,7 +1,11 @@
 function showMenu() {
+  let mobileview = document.querySelector(".mobile-access-btn");
+  let data = document.getElementById("nav");
   let mobileMenu = document.querySelector(".mobile-menu");
   if (mobileMenu.style.display == "none") {
     mobileMenu.style.display = "block";
+    mobileview.innerHTML=null;
+    mobileview.append(data);
   } else {
     mobileMenu.style.display = "none";
   }
